f91c8264ea9c868d7c9b064604560753f9107dce
Ben Neuwirth <bmneuwirth@gmail.com>
2021-11-16 15:53:38 -0500

    initial commit

---
+++ Pods/Target Support Files/Pods-hackchallenge/Pods-hackchallenge-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hackchallenge : NSObject
+@end
+@implementation PodsDummy_Pods_hackchallenge
+@end
---
+++ hackchallenge/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  hackchallenge
+//
+//  Created by Ben Neuwirth on 11/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Hack Challenge"
+        view.backgroundColor = .white
+    }
+
+
+}
+
